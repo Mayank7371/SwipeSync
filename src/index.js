c608b0517cbcc52466a65c688c4cd5863279d73c
Mayank7371 <neprone.dev@gmail.com>
2025-01-24 01:54:54 +0530

    updste index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+  res.send("Hello World!");
+  next();
+});
+app.post("/", (req, res, next) => {
   res.send("Hello World!");
 });
 app.listen(port, () => {
